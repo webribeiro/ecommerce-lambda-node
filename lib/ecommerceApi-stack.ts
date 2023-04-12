4300340a03f93a34c173b91417c195c8a393326f
webribeiro <gwebribeiro@gmail.com>
2023-04-12 14:18:05 -0300

    stack products get e api gtw

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from "aws-cdk-lib"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as cwlogs from "aws-cdk-lib/aws-logs"
+import { Construct } from "constructs"
+
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
+        super(scope, id, props)
+
+        const logGroup = new cwlogs.LogGroup(this, "EcommerceApiLogs")
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "EcommerceApi",
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
+        })
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+        const productsResource = api.root.addResource("products")
+        productsResource.addMethod("GET", productsFetchIntegration)
+    }
+}
