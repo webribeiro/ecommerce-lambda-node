a3c035ac1c9fbcd45edd4a34b9becb0dad96bb42
webribeiro <gwebribeiro@gmail.com>
2023-04-17 21:30:28 -0300

    update metods post, put e delete

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -1,32 +1,61 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer"
+import { DynamoDB } from "aws-sdk"
 
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> { 
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
     const lambdaRequestId = context.awsRequestId
     const apiRequestId = event.requestContext.requestId
-    
+
     console.log(`API Gateway RequestId ${apiRequestId} - Lambda RequestId ${lambdaRequestId}`)
 
-    if(event.resource === "/products") {
+    if (event.resource === "/products") {
         console.log("POST /products")
+        const product = JSON.parse(event.body!) as Product
+        const productCreated = await productRepository.create(product)
+
         return {
             statusCode: 201,
-            body: "POST /products"
+            body: JSON.stringify(productCreated)
         }
     } else if (event.resource == "/products/{id}") {
         const productId = event.pathParameters!.id as string
 
-        if(event.httpMethod === "PUT") {
+        if (event.httpMethod === "PUT") {
             console.log(`PUT /products/${productId}`)
-            return {
-                statusCode: 200,
-                body: `PUT /products/${productId}`
+            const product = JSON.parse(event.body!) as Product
+            try {
+                const productUpdate = await productRepository.update(productId, product)
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(productUpdate)
+                }
+            } catch (ConditionalCheckFailedException) {
+                return {
+                    statusCode: 404,
+                    body: 'Product not found'
+                }
             }
-        }else if(event.httpMethod === "DELETE") {
+
+        } else if (event.httpMethod === "DELETE") {
             console.log(`DELETE /products/${productId}`)
-            return {
-                statusCode: 200,
-                body: `DELETE /products/${productId}`
+            try {
+                const product = await productRepository.delete(productId)
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(product)
+                }
+            } catch (error) {
+                console.error((<Error>error).message)
+                return {
+                    statusCode: 404,
+                    body: (<Error>error).message
+                }
             }
         }
     }
