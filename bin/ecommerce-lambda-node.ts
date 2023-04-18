f7a969231fdb6bc8a1509dec2a7ce6348f619389
webribeiro <gwebribeiro@gmail.com>
2023-04-17 21:51:57 -0300

    correção add dependencia appStack

--- bin/ecommerce-lambda-node.ts
+++ bin/ecommerce-lambda-node.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProductsAppStack } from '../lib/productsApp-stack';
 import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
-import { ProductsAppLayersStack } from '../lib/productsApplayers-stack';
+import { ProductsAppLayersStack } from '../lib/productsApplayers-stack'; 
 
 const app = new cdk.App();
 
@@ -26,6 +26,8 @@ const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
   tags: tags,
   env: env
 })
+
+productsAppStack.addDependency(productsAppLayersStack)
 
 const ecommerceApiStack = new EcommerceApiStack(app, "EcommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
